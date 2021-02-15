828884f29f6818de1652275cf5136bdabbc92a04
Jefferson Lima <chrisjefflima@gmail.com>
2021-02-15 11:10:36 -0300

    Projeto iniciado

---
+++ Novo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Novo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
